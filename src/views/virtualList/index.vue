cc99a2039c98fadba72cb1bb2f9db2f51e0b6bdf
ljy <4175459@qq.com>
2024-09-18 13:34:23 +0800

    fix: 添加虚拟列表说明

---
+++ src/views/virtualList/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref,nextTick } from "vue";
+import dayjs from "dayjs"
+
+const listArr = ref([]);
+const addDomHandler = async () => {
+  // 记录任务开始时间
+  let now = Date.now();
+  listArr.value = new Array(10000).fill(Math.random());
+  console.log('js运行后时间：', Date.now() - now)
+  await nextTick(); // 等待DOM更新
+  console.log('总运行时间：', Date.now() - now)
+};
+</script>
+
+<template>
+  <el-button @click="addDomHandler">添加dom</el-button>
+  <ul>
+    <li v-for="item in listArr" :key="item">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<style></style>
